19a7beaaff658a2e04df612ff5a39baa8e9abd61
Mohammad Jamali <mohammadjamalid@gmail.com>
2024-02-20 18:00:48 +0330

    Project Codebase Is Prepared

---
+++ tests/Application.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Application.FunctionalTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Web.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Web.AcceptanceTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
